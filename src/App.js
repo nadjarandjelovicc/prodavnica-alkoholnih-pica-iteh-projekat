b2838fa18cbbbfbf306a282aa17194742c11217b
Kristina36916 <kristina.ivancuk@gmail.com>
2022-09-03 23:20:05 +0200

    Dodata komponenta futer

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
  
 import './App.css';
+import Footer from './Komponente/Footer';
 import NavBar from './Komponente/NavBar';
 
 function App() {
   return (
     <div  >
         <NavBar></NavBar>
+        <Footer></Footer>
     </div>
   );
 }
