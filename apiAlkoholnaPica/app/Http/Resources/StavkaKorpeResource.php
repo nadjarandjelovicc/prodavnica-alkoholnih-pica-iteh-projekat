6b1342ea2290f916349d3ecbda1879a5077a7285
MarePet <markopetrovic753@gmail.com>
2022-09-03 22:26:15 +0200

    Kreirani kontroleri za model, kao i resource-i

---
+++ apiAlkoholnaPica/app/Http/Resources/StavkaKorpeResource.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Http\Resources;
+
+use App\Models\Pice;
+use App\Models\Proizvod;
+use App\Models\User;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class StavkaKorpeResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array|\Illuminate\Contracts\Support\Arrayable|\JsonSerializable
+     */
+    public function toArray($request)
+    {
+        return  [
+            'korpa' => $this->resource->korpa_id,
+            'pice' => new PiceResource(Pice::find($this->resource->proizvod_id)),
+            'user' => new UserResource (User::find($this->resource->user_id)),
+            'kolicina' => $this->resource->kolicina,
+
+          
+
+
+        ];
+    }
+}
