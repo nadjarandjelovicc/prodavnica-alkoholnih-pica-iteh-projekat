76c8d51b06f747a6c03ed5215ca18b09a273f542
MarePet <markopetrovic753@gmail.com>
2022-09-03 22:50:37 +0200

    Ispravljena greska  u kontroleru

--- apiAlkoholnaPica/app/Http/Controllers/PiceController.php
+++ apiAlkoholnaPica/app/Http/Controllers/PiceController.php
@@ -2,12 +2,14 @@
 
 namespace App\Http\Controllers;
 
-use App\Http\Resources\ProizvodResource;
-use App\Models\Proizvod;
+use App\Http\Resources\PiceResource;
+ 
+use App\Models\Pice;
+ 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Validator;
 
-class ProizvodController extends Controller
+class PiceController extends Controller
 {
     /**
      * Display a listing of the resource.
@@ -16,7 +18,7 @@ class ProizvodController extends Controller
      */
     public function index()
     {
-        return ProizvodResource::collection(Proizvod::all());
+        return PiceResource::collection(Pice::all());
     }
 
     /**
@@ -40,10 +42,10 @@ class ProizvodController extends Controller
         $validator = Validator::make(
             $request->all(),
             [
-                'name' =>  'required' , 
-                'description' => 'required|string|max:100', 
-                'price' => 'required',
-                'category' => 'required'  ,
+                'naziv' =>  'required' , 
+                'proizvodjac' => 'required|string|max:100', 
+                'cena' => 'required',
+                'vrsta' => 'required'  ,
                 'image' =>'  '
 
             ]
@@ -55,38 +57,38 @@ class ProizvodController extends Controller
          
             
 
-        $p = Proizvod::create([
-                'name' =>   $request->name, 
-                'description' => $request->description, 
-                'price' =>  $request->price, 
-                'tezina'=>$request->tezina, 
-                'kamera'=>$request->kamera, 
+        $p = Pice::create([
+                'naziv' =>   $request->naziv, 
+                'proizvodjac' => $request->proizvodjac, 
+                'cena' =>  $request->cena, 
+                'zemlja_porekla'=>$request->zemlja_porekla, 
+                'kolicina'=>$request->kolicina, 
 
-                'category' =>  $request->category, 
+                'vrsta' =>  $request->vrsta, 
                 'image' =>  $request->image
            
         ]);
-        return response()->json(["Uspesno kreiran proizvod",$p]);
+        return response()->json(["Uspesno kreirano pice",$p]);
     }
 
     /**
      * Display the specified resource.
      *
-     * @param  \App\Models\Proizvod  $proizvod
+     * @param  \App\Models\Pice  $pice
      * @return \Illuminate\Http\Response
      */
     public function show($id)
     {
-        return new ProizvodResource(Proizvod::find($id));
+        return new PiceResource(Pice::find($id));
     }
 
     /**
      * Show the form for editing the specified resource.
      *
-     * @param  \App\Models\Proizvod  $proizvod
+     * @param  \App\Models\Pice  $pice
      * @return \Illuminate\Http\Response
      */
-    public function edit(Proizvod $proizvod)
+    public function edit(Pice $pice)
     {
         //
     }
@@ -95,7 +97,7 @@ class ProizvodController extends Controller
      * Update the specified resource in storage.
      *
      * @param  \Illuminate\Http\Request  $request
-     * @param  \App\Models\Proizvod  $proizvod
+     * @param  \App\Models\Pice  $pice
      * @return \Illuminate\Http\Response
      */
     public function update(Request $request)
@@ -108,26 +110,20 @@ class ProizvodController extends Controller
         if ($validator->fails()) 
             return response()->json($validator->errors());
 
-        $l=Proizvod::find($request->id);
+        $l=Pice::find($request->id);
         if($l){
-            $l->name = $request->name;
-            $l->description = $request->description;
-            $l->price = $request->price;
-            $l->category  =1;
+            $l->naziv = $request->naziv;
+            $l->proizvodjac = $request->proizvodjac;
+            $l->cena = $request->cena;
+            $l->vrsta  =1;
             $l->image = $request->image;
-            $l->kamera = $request->kamera;
-            $l->tezina = $request->tezina;
-            $l->amount =0;
-            $l->uporedi =0;
-            $l->memorija =4;
-            $l->baterija =4000;
-
-
-
+            $l->zemlja_porekla = $request->zemlja_porekla; 
+            $l->kolicina =0;
+           
 
             
             $l->save();
-            return response()->json(['Proizvod uspesno izmenjen!', new ProizvodResource($l)]);
+            return response()->json(['Pice uspesno izmenjeno!', new PiceResource($l)]);
         }else{
             return response()->json('Trazeni objekat ne postoji u bazi');
         }
@@ -136,12 +132,12 @@ class ProizvodController extends Controller
     /**
      * Remove the specified resource from storage.
      *
-     * @param  \App\Models\Proizvod  $proizvod
+     * @param  \App\Models\Pice  $pice
      * @return \Illuminate\Http\Response
      */
     public function destroy($id)
     {
-        $p = Proizvod::find($id);
+        $p = Pice::find($id);
         if($p){ 
             $p->delete();
             return response()->json("uspesno obrisano!" );
