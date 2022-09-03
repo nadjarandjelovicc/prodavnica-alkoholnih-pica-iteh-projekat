b76f0cd41024f354b060dd8c84f5f1244a73a817
Kristina36916 <kristina.ivancuk@gmail.com>
2022-09-03 23:33:17 +0200

    Dodata pocetna stranica

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@
 import './App.css';
 import Footer from './Komponente/Footer';
 import NavBar from './Komponente/NavBar';
+import Pocetna from './Komponente/Pocetna';
 
 function App() {
   return (
     <div  >
         <NavBar></NavBar>
+        <Pocetna></Pocetna>
         <Footer></Footer>
     </div>
   );
--- src/Komponente/Pocetna.jsx
+++ src/Komponente/Pocetna.jsx
@@ -1,12 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { Slide } from 'react-slideshow-image';
+import 'react-slideshow-image/dist/styles.css'
+ 
 
-function Pocetna() {
-  return (
-    <div className="App">
-    cd
-    </div>
-  );
-}
+
+const slideImages = [
+  'https://arc-anglerfish-arc2-prod-advancelocal.s3.amazonaws.com/public/ELBFECKDMNGRXGGIMDFLUFT3CU.jpg',
+  'https://c1.wallpaperflare.com/preview/755/538/407/drink-store-wine.jpg',
+  'https://images.pexels.com/photos/260922/pexels-photo-260922.jpeg?cs=srgb&dl=pexels-pixabay-260922.jpg&fm=jpg'
+];
+
+const Pocetna = () => {
+ 
+
+
+
+
+    return (
+
+      <div> 
+        <Slide easing="ease">
+          <div className="each-slide">
+            <div style={{'backgroundImage': `url(${slideImages[0]})`}}>
+            
+            </div>
+          </div>
+          <div className="each-slide">
+            <div style={{'backgroundImage': `url(${slideImages[1]})`}}>
+            
+            </div>
+          </div>
+          <div className="each-slide">
+            <div style={{'backgroundImage': `url(${slideImages[2]})`}}>
+             
+            </div>
+          </div>
+        </Slide>
+      </div>
+    )
+};
 
 export default Pocetna;
